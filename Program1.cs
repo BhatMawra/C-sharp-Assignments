4514d6d633e8dfe8b6b4a609de8c88e2a0a59d77
BhatMawra <bhatmawra@gmail.com>
2024-05-17 16:51:23 +0530

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿// Assignment 2: Write a C# program to swap two numbers
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine("Before swap:");
+        Console.WriteLine($"a = {a}, b = {b}");
+
+        // Swap logic
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("After swap:");
+        Console.WriteLine($"a = {a}, b = {b}");
+    }
+}
+
+
